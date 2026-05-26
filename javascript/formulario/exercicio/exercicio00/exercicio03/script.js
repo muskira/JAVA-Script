bb8f88c1f4b9f814da945f48ee86a038c8c14138
Fabiano Nicolodelli da Silva Chagas <muskira@gmail.com>
2026-05-26 20:46:01 -0300

    exercio03

---
+++ javascript/formulario/exercicio/exercicio00/exercicio03/script.js
@@ -0,0 +1,27 @@
+const formulario = document.getElementById("formulario");
+    const email = document.getElementById("email");
+    const mensagem = document.getElementById("mensagem");
+
+    formulario.addEventListener("submit", function(event) {
+      event.preventDefault();
+
+      const valorEmail = email.value;
+
+      // Verifica se contém @ e .
+      if (valorEmail.includes("@") && valorEmail.includes(".")) {
+
+        email.classList.add("input-valido");
+        email.classList.remove("input-invalido");
+
+        mensagem.textContent = "E-mail válido!";
+        mensagem.className = "sucesso";
+
+      } else {
+
+        email.classList.add("input-invalido");
+        email.classList.remove("input-valido");
+
+        mensagem.textContent = "E-mail inválido";
+        mensagem.className = "erro";
+      }
+    });
