737ad6cd319b698b7396204ef5c4f58d1674617e
Fabiano Nicolodelli da Silva Chagas <muskira@gmail.com>
2026-05-26 21:52:55 -0300

    exercio04

--- javascript/formulario/exercicio/exercicio00/exercicio02/script.js
+++ javascript/formulario/exercicio/exercicio00/exercicio02/script.js
@@ -5,11 +5,11 @@ const formulario = document.getElementById("formulario");
     formulario.addEventListener("submit", function(event) {
       event.preventDefault(); // não recarrega a página
 
-      if (nome.value === "") {
-        nome.classList.add("borda-vermelha");
+      if (formulario.nome.value === "") {
+        formulario.nome.classList.add("borda-vermelha");
         erroNome.textContent = "Campo obrigatório";
       } else {
-        nome.classList.remove("borda-vermelha");
+        formulario.nome.classList.remove("borda-vermelha");
         erroNome.textContent = "";
       }
     });
