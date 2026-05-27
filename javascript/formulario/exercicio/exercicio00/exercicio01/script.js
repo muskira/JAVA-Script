dc9d66f6324bce3265fcc23d495938e8e5add2d7
Fabiano Nicolodelli da Silva Chagas <muskira@gmail.com>
2026-05-26 21:11:42 -0300

    exercio04

--- javascript/formulario/exercicio/exercicio00/exercicio01/script.js
+++ javascript/formulario/exercicio/exercicio00/exercicio01/script.js
@@ -3,10 +3,10 @@
     const formulario = document.getElementById("meuFormulario");
     
     // Adiciona o evento de submit
-    formulario.addEventListener("submit", function(evento) {
+    formulario.addEventListener("submit", function(event) {
     
     // Impede o recarregamento da página
-    evento.preventDefault();
+    event.preventDefault();
     
     // Exibe a mensagem
     alert("Formulário interceptado com sucesso!");
