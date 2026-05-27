a737b4c1144f644b71ca817eabe10b2aa1846fb9
Fabiano Nicolodelli da Silva Chagas <muskira@gmail.com>
2026-05-26 21:27:07 -0300

    exercio04

--- javascript/formulario/exercicio/exercicio00/exercicio02/script.js
+++ javascript/formulario/exercicio/exercicio00/exercicio02/script.js
@@ -3,7 +3,7 @@ const formulario = document.getElementById("formulario");
     const erroNome = document.getElementById("erroNome");
 
     formulario.addEventListener("submit", function(event) {
-      event.preventDefault();
+      event.preventDefault(); // não recarrega a página
 
       if (nome.value === "") {
         nome.classList.add("borda-vermelha");
